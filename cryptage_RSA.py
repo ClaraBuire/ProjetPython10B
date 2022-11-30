e739de5e87c46b57407b7df6544525716e01a458
wenlixu16 <wenlix116@gmail.com>
2022-11-30 18:01:57 +0100

    ajout des commentaires

---
+++ cryptage_RSA.py
@@ -0,0 +1,9 @@
+from Crypto.PublicKey import RSA
+
+#Le chiffrement RSA est asymétrique : il utilise une paire de clés (des nombres entiers)
+#  composée d'une clé publique pour chiffrer et d'une clé privée pour déchiffrer des données confidentielles.
+#Une condition indispensable est qu'il soit « calculatoirement impossible » de déchiffrer à l'aide de la seule clé publique
+
+key = RSA.generate(2048) # key contient la clé pivée
+# key.publickey() donne la clé public
+print(key)
